ebec9fa878967cbe0a22b91983a014b1f8e5f809
jmss29-collab <jmss29@discente.ifpe.edu.br>
2025-12-12 19:07:37 -0300

    Core

--- urls.py
+++ urls.py
@@ -1,7 +1,47 @@
+"""
+URL configuration for core project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
 from django.urls import path
-from .views import LocalizacaoListCreate, LocalizacaoDetail
 
 urlpatterns = [
-    path('localizacoes/', LocalizacaoListCreate.as_view(), name='localizacao-list'),
-    path('localizacoes/<int:pk>/', LocalizacaoDetail.as_view(), name='localizacao-detail'),
+    path('admin/', admin.site.urls),
+]
+
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API de Localização",
+        default_version="v1",
+        description="Documentação da API de Localização",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+
+    # Swagger
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc'),
 ]
